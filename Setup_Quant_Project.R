87eb589ce1048a2d196286d3955aae8b9aad564d
Snat3r <40038398+Snat3r@users.noreply.github.com>
2019-02-28 17:15:21 +0100

    Add files via upload

---
+++ Setup_Quant_Project.R
@@ -0,0 +1,11 @@
+##################
+#    Created By: #
+#     Alex       #
+#    Berkout     #
+# AND            #
+#     Coen       #
+#   van der      #
+#     Geest      #
+##################
+
+QP4 = read.csv('D://Quant_Data/google_play_data.csv', stringsAsFactors = FALSE, sep = ";")
