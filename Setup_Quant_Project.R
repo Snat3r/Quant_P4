aff2f5f301d3cabce35f6e4ae097cf9892b44ba0
Snat3r <40038398+Snat3r@users.noreply.github.com>
2019-02-28 17:39:36 +0100

    Update Setup_Quant_Project.R

--- Setup_Quant_Project.R
+++ Setup_Quant_Project.R
@@ -8,4 +8,13 @@
 #     Geest      #
 ##################
 
+# Load csv file into R/ change directory if necessary
 QP4 = read.csv('D://Quant_Data/google_play_data.csv', stringsAsFactors = FALSE, sep = ";")
+
+#If needed remove NA values from dataset
+final[complete.cases(final), ]
+
+#gives an overview of the df, type of values in a column 
+    # app_version and price need attention
+        # both seen as character needs to be changed to numerical (some values currently contain dollarsigns $)
+str(QP4)
