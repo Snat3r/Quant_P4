5d2079f7a7594bb2991297f89a6860f73bb37408
Snat3r <40038398+Snat3r@users.noreply.github.com>
2019-03-01 09:39:01 +0100

    Update Setup_Quant_Project.R

--- Setup_Quant_Project.R
+++ Setup_Quant_Project.R
@@ -8,6 +8,8 @@
 #     Geest      #
 ##################
 
+library(tidyverse)
+
 # Load csv file into R/ change directory if necessary
 QP4 = read.csv('D://Quant_Data/google_play_data.csv', stringsAsFactors = FALSE, sep = ";")
 
@@ -18,3 +20,12 @@ final[complete.cases(final), ]
     # app_version and price need attention
         # both seen as character needs to be changed to numerical (some values currently contain dollarsigns $)
 str(QP4)
+
+#Remove + at the end of the intalls column
+QP4$intalls = sub('[+]', '', QP4$intalls)
+
+#Remove dollar sign at the beginning of price column
+QP4$price = sub('[$]', '', QP4$price)
+
+#replaces 'varies with device' in the file_sice and app_version to NA
+QP4[QP4 == 'Varies with device'] <- NA
