2fd64671c09fdc509e0e389bed7900eccc1241b7
Snat3r <40038398+Snat3r@users.noreply.github.com>
2019-03-01 10:06:44 +0100

    Update Setup_Quant_Project.R

--- Setup_Quant_Project.R
+++ Setup_Quant_Project.R
@@ -2,9 +2,9 @@
 #    Created By: #
 #     Alex       #
 #    Berkout     #
-#      AND       #
+# AND            #
 #     Coen       #
-#    van der     #
+#   van der      #
 #     Geest      #
 ##################
 
@@ -15,7 +15,7 @@ library(lubridate)
 QP4 = read.csv('D://Quant_Data/google_play_data.csv', stringsAsFactors = FALSE, sep = ";")
 
 #If needed remove NA values from dataset
-final[complete.cases(final), ]
+#final[complete.cases(final), ]
 
 #gives an overview of the df, type of values in a column 
     # app_version and price need attention
@@ -28,12 +28,13 @@ QP4$intalls = sub('[+]', '', QP4$intalls)
 #Remove dollar sign at the beginning of price column
 QP4$price = sub('[$]', '', QP4$price)
 
-#Remove 'M' at the end of file size (9.9M) to (9.9)
+#Remove 'M' and 'k' at the end of file size (9.9M) to (9.9)
 QP4$file_size = sub('[M]', '', QP4$file_size)
+QP4$file_size = sub('[k]', '', QP4$file_size)
 
-#replaces 'varies with device' in the file_sice and app_version to NA
+#replaces 'varies with device' in the file_size and app_version to NA And change NaN in Rating to NA
 QP4[QP4 == 'Varies with device'] <- NA
+QP4$rating[QP4$rating == 'NaN'] <- NA
 
 #convert last updated into date variable
 QP4$last_updated = as.Date(mdy(QP4$last_updated))
-
